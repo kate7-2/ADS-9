e1178caec65095d9dad491c65cb8bbfb3212a6c9
kate7-2 <katemate484@gmail.com>
2025-06-07 02:36:49 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -33,4 +33,4 @@ std::vector<std::vector<char>> getAllPerms(PMTree& tree);
 std::vector<char> getPerm1(PMTree& tree, int num);
 std::vector<char> getPerm2(PMTree& tree, int num);
 
-#endif  
+#endif
